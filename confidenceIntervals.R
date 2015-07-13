d1e739b38f122cca4c7d39ddaa6f4c5012bbda68
drasnop <ponsard.antoine@gmail.com>
2015-07-13 12:24:49 -0700

    filter out first two trials, because of high learning effect

--- confidenceIntervals.R
+++ confidenceIntervals.R
@@ -6,11 +6,14 @@ source("helpers.R")
 # import data from csv
 data <- read.csv("C:/Users/Antoine/programmation/web/stencil-analysis/1-20-pax/trials-1-20-pax.csv")
 
+# make sure the independent variable is a factor
+data$interface <- factor(data$interface)
+
 # log-transform durations, to obtain a normal distribution
 data$shortDuration <- log(1+data$shortDuration)
 
-# make sure the independent variable is a factor
-data$interface <- factor(data$interface)
+# filter out first two trials, because of high learning effect
+data <- subset(data, trialNumber >= 2)
 
 # data is already in the "long" format
 # if it were in the "wide" format, use tidyr gather() or reshape2 melt()
